2c8389186290d4d4fcd068774c145b7d620abe0b
lxg <1436215718@qq.com>
2019-05-07 17:19:43 +0800

    Serilog

--- Heavy.Web/Program.cs
+++ Heavy.Web/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO;
 using Heavy.Web.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
 
 namespace Heavy.Web
 {
@@ -10,6 +13,14 @@ namespace Heavy.Web
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine("Logs", "log.txt"), rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             var host = CreateWebHostBuilder(args).Build();
             using (var scope = host.Services.CreateScope())
             {
@@ -31,6 +42,7 @@ namespace Heavy.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseStartup<Startup>();
     }
 }
